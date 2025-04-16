9ed8fbc070dd8f630db83db8ee6dcf4db46118d7
Chojungseok <whwndtjr9712@naver.com>
2025-04-17 00:28:35 +0900

    data insert 성공/upsert 필요

--- sejong_gcoo_location_api.py
+++ sejong_gcoo_location_api.py
@@ -2,6 +2,7 @@ import os
 import requests
 from dotenv import load_dotenv
 from pprint import pprint
+import psycopg2
 
 load_dotenv()
 
@@ -27,8 +28,41 @@ requests_url = f'{url}/{get_path}'
 res = requests.get(requests_url, params=payload)
 res = res.json()
 total_count = res['response']['body']['totalCount']
+datas = res['response']['body']['items']['item']
+pprint(datas[:5])
 
-# print(total_count)
-# pprint(res['response']['body']['totalCount'])
-pprint(res['response']['body']['items']['item'][0])
+
+
+# postgresql 연결
+
+# 데이터베이스 연결 설정
+try:
+    connection = psycopg2.connect(
+        dbname="sejong",  # 데이터베이스 이름
+        user="chojungseok",         # PostgreSQL 사용자 이름
+        password="jungseok0324!",     # 사용자 비밀번호
+        host="localhost",             # 데이터베이스 호스트 (기본값은 localhost)
+        port="5432"                   # PostgreSQL 포트 (기본값은 5432)
+    )
+    
+    # 연결이 성공하면 커서 생성
+    cursor = connection.cursor()
+    
+    insert_query = """
+        insert into GBIKE (battery, citycode, cityname, latitude, longitude, providername, vehicleid)
+        values(%s, %s, %s, %s, %s, %s, %s)
+    """
+
+    for data in datas:
+        cursor.execute(insert_query, (data['battery'], data['citycode'], data['cityname'], data['latitude'], data['longitude'], data['providername'], data['vehicleid']))
+
+    connection.commit()
+except Exception as error:
+    print('error 발생', error)
+finally:
+    # 연결 종료
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Connection closed.")
 
