6b609c341dce59ec7c0214be350cfddef9dfcbcc
Chojungseok <whwndtjr9712@naver.com>
2025-04-16 23:15:57 +0900

    postgre/postgis install

--- sejong_gcoo_location_api.py
+++ sejong_gcoo_location_api.py
@@ -9,13 +9,13 @@ url  = 'http://apis.data.go.kr/1613000/PersonalMobilityInfoService'
 get_path = 'getPMListByProvider'
 
 API_KEY = os.environ['API_KEY']
-pageNo = 1500
+numOfRows = 2000
 provider_name = 'GBIKE'
 city_code = 12
 
 payload = {
     'serviceKey': API_KEY,
-    'pageNo': pageNo,
+    'numOfRows': numOfRows,
     'providerName': provider_name, 
     'cityCode': city_code,
     '_type': 'json'
@@ -27,6 +27,8 @@ requests_url = f'{url}/{get_path}'
 res = requests.get(requests_url, params=payload)
 res = res.json()
 total_count = res['response']['body']['totalCount']
+
+# print(total_count)
 # pprint(res['response']['body']['totalCount'])
-# pprint(len(res['response']['body']['items']['item']))
+pprint(res['response']['body']['items']['item'][0])
 
