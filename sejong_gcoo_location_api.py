2f89c5c54ad380a4c261d68654edbdabbc412e03
Chojungseok <whwndtjr9712@naver.com>
2025-04-16 17:21:18 +0900

    while data 호출/ 일괄호출 수정 필요

--- sejong_gcoo_location_api.py
+++ sejong_gcoo_location_api.py
@@ -9,11 +9,13 @@ url  = 'http://apis.data.go.kr/1613000/PersonalMobilityInfoService'
 get_path = 'getPMListByProvider'
 
 API_KEY = os.environ['API_KEY']
+pageNo = 1500
 provider_name = 'GBIKE'
 city_code = 12
 
 payload = {
     'serviceKey': API_KEY,
+    'pageNo': pageNo,
     'providerName': provider_name, 
     'cityCode': city_code,
     '_type': 'json'
@@ -23,4 +25,8 @@ payload = {
 requests_url = f'{url}/{get_path}'
 
 res = requests.get(requests_url, params=payload)
-pprint(res.json())
+res = res.json()
+total_count = res['response']['body']['totalCount']
+# pprint(res['response']['body']['totalCount'])
+# pprint(len(res['response']['body']['items']['item']))
+
